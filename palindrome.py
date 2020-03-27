1ab14c74ddb27c982a9f30de02b9389ea83e9087
Alex <54078844+AZelentsov343@users.noreply.github.com>
2020-03-28 02:21:24 +0300

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -47,34 +47,26 @@ class Solution2: # O(n^2) Expanding from points
 
 class Solution3: #O(n) Manacher
 
-  def expand_borders_odd(self, s, left, right):
+  def expand_borders(self, s, left, right, odd):
     if left < 0 or right >= len(s):
-        return 1
-    while left - 1 >= 0 and right + 1 < len(s) and s[left - 1] == s[right + 1]:
-      left -= 1
-      right += 1
-    return right - left + 1
-
-  def expand_borders_even(self, s, left, right):
-    if left < 0 or right >= len(s) or s[left] != s[right]:
+      return int(odd)
+    if (not odd) and s[left] != s[right]:
       return 0
     while left - 1 >= 0 and right + 1 < len(s) and s[left - 1] == s[right + 1]:
       left -= 1
       right += 1
-    return right - left + 1
+    return (right - left + 1)//2
 
   def odd_substr(self, s):
     d = [0] * len(s)
     left = 0
     right = -1
     for i in range(len(s)):
-      if i > right:
-        d[i] = self.expand_borders_odd(s, i, i)
-      else:
-        j = right - i + left
-        offset = min(right - i, d[j])
-        d[i] = self.expand_borders_odd(s, i - offset, i + offset)
-      if (i + d[i]//2 > right):
+      k = 0
+      if i <= right:
+        k = min(right - i, d[right - i + left])
+      d[i] = self.expand_borders(s, i - k, i + k, True)
+      if (i + d[i] > right):
         left = i - d[i]
         right = i + d[i]
     return np.max(d), np.argmax(d)
@@ -87,12 +79,10 @@ class Solution3: #O(n) Manacher
       k = 0
       if i <= right:
         k = min(right - i + 1, d[right - i + left + 1])
-      while i + k <= len(s) and i - k - 1 > 0 and s[i + k] == s[i - k - 1]:
-        k += 1
-      d[i] = 2*k
-      if i + k - 1 > right:
-        left = i - k
-        right = i + k - 1
+      d[i] = self.expand_borders(s, i - k - 1, i + k, False)
+      if i + d[i] - 1 > right:
+        left = i - d[i]
+        right = i + d[i] - 1
     return np.max(d), np.argmax(d)
 
   
@@ -100,9 +90,9 @@ class Solution3: #O(n) Manacher
     odd_max, odd_num = self.odd_substr(s)
     even_max, even_num = self.even_substr(s)
     if odd_max >= even_max:
-      return s[odd_num-odd_max//2:odd_num+odd_max//2+1]
+      return s[odd_num-odd_max:odd_num+odd_max+1]
     else:
-      return s[even_num-even_max//2:even_num+even_max//2]
+      return s[even_num-even_max:even_num+even_max]
 
         
 # Test program
