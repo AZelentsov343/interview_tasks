b9ce16541babe106c4d64e0a3fa99511c2c309b4
Alex <54078844+AZelentsov343@users.noreply.github.com>
2020-03-29 01:25:48 +0300

    Create sort_formatted_strings.py

---
+++ sort_formatted_strings.py
@@ -0,0 +1,10 @@
+#Given a list of string formatted like '{substring}_{number}.{something}'
+#Sort them descending by {substring} and then ascending by {number}
+
+l = ['example_1.txt', 'example_2.tar', 'abc_1.py']
+
+class Solution:
+  def sort(self, l):
+    return sorted(l, key=lambda s: (s.split('_')[0], -int(s.split('_')[1].split('.')[0])))
+
+print(Solution().sort(l))
