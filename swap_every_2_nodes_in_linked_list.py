c510691ffa4af821d97f811d23e8370e1f2207ab
Alex <54078844+AZelentsov343@users.noreply.github.com>
2020-08-06 18:16:49 +0300

    Create swap_every_2_nodes_in_linked_list.py

---
+++ swap_every_2_nodes_in_linked_list.py
@@ -0,0 +1,27 @@
+#Given a linked list, swap the position of the 1st and 2nd node, then
+#swap the position of the 3rd and 4th node etc.
+
+
+class Node:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+  def __repr__(self):
+    return f"{self.value}, ({self.next.__repr__()})"
+
+
+def swap_every_two(llist):
+  if llist is None:
+    return None
+  if llist.next is None:
+    return llist
+  res = llist.next
+  after = swap_every_two(llist.next.next)
+  llist.next.next = llist
+  llist.next = after
+  return res
+
+llist = Node(1, Node(2, Node(3, Node(4, Node(5)))))
+print(swap_every_two(llist))
+# 2, (1, (4, (3, (5, (None)))))
